e74958a7cb442012a90f6fd15ea261a0713d3a23
nymbus <xxvmelanconxx@gmail.com>
2023-11-21 19:52:07 +0100

    Discord login logic implemented

--- ync-node-app/index.js
+++ ync-node-app/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const crypto = require('crypto');
+
+const fs = require('fs');
 
 const passport = require('passport');
 const DiscordStrategy = require('passport-discord');
@@ -7,15 +10,16 @@ const DiscordStrategy = require('passport-discord');
 const secretData = require('./little-secrets-shall-be-kept.json');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Connect to DB
 
 // Set up sessions
 app.use(session({
-    secret: 'session-secret',
+    secret: crypto.randomBytes(32).toString('hex'),
     resave: false,
     saveUninitialized: false,
+    cookie: {secure: true}
 }));
 
 // Initialize passport
@@ -26,32 +30,38 @@ app.use(passport.session());
 passport.use(new DiscordStrategy({
     clientID: secretData.discord.id,
     clientSecret: secretData.discord.secret,
-    callbackURL: 'http://localhost:3000/auth/discord/callback',
+    callbackURL: 'http://localhost:3001/auth/discord/callback',
     scope: 'identify'
 }, async (accessToken, refreshToken, profile, done) => {
     // Handle the user profile from Discord and save it to the database
-    const user = undefined;
-    console.log(profile.id, profile.username);
-    return done(null, user);
+    const user = JSON.stringify(profile, null, 2);
+    fs.writeFile('./user.json', user, (err) => { console.log(err); });
+    return done(null, profile);
 }));
 
 // Serialize/deserialize user for sessions
-passport.serializeUser((user, done) => done(null, user.id));
+passport.serializeUser((user, done) => {
+    console.log('serializing...');
+    console.log(user.id);
+    done(null, user.id);
+});
 passport.deserializeUser(async (id, done) => {
-    const user = undefined;
-    done(null, user);
+    // const user = undefined;
+    console.log('deserializing...');
+    done(null, id);
 });
 
 // Routes
 app.get('/', (req, res) => { 
+    console.log(session.secret);
     res.sendFile('./html/index.html', {root: __dirname});
 });
-
 app.get('/auth/discord', passport.authenticate('discord'));
 app.get('/auth/discord/callback',
-    passport.authenticate('discord', { failureRedirect: '/' }), (req, res) => {
+    passport.authenticate('discord', { failureRedirect: '/?failed=true' }), (req, res) => {
         // Successful authentication, redirect to the home page or another route
-        res.redirect('/');
+        console.log(session.secret);
+        res.redirect('/?failed=false');
     }
 );
 
