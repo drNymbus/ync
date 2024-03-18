3a7e4c514a7d5796d5e9a8fbc6834d0edbaf676a
nymbus <xxvmelanconxx@gmail.com>
2024-03-18 20:51:15 +0100

    Began connecting to cassandra cluster and integrating cookie authentification

--- ync-node-app/index.js
+++ ync-node-app/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const fs = require('fs');
 const cassandra = require('cassandra-driver');
+const cookieParser = require('cookie-parser');
 
 // Set up express app
 const app = express();
@@ -7,28 +9,38 @@ const port = process.env.PORT || 3001;
 
 // Connect to CassandraDB
 const client = new cassandra.Client({
-    contactPoints: ['127.0.0.1:9042'], // NO ABSOLUTE PATH, this should be an argument ?
+    contactPoints: [process.env.CASSANDRA_CONTACT_POINTS || '127.0.0.1'],
     localDataCenter: 'spade',
     keyspace: 'store'
 });
 
+app.use(cookieParser()); // Hand over the secret string for signed cookies
+
 // Routes
 app.get('/', (req, res) => {
-    // Get cookie in req
+    console.log('Hi from /' + req.cookies);
+    client.execute('SELECT * FROM store.basket;')
+        .then(result => console.log(result));
+      // Get cookie in req
     // If cookie in DB
         // Return all basket data
     // If no cookie in DB
         // Create new basket in DB with associated cookie
 });
 
-app.get('/basket/insert', (req, res) => {
-    // Get all information in request body
-    // Format all information in CQL
-    // Apply CQL transaction to DB
+app.post('/basket/insert', (req, res) => {
+    console.log('Hi from /basket/insert' + req.cookies);
+    client.execute('SELECT * FROM store.basket;')
+        .then(result => console.log(result));
+    // Get cookie in req
 });
 
-app.get('/basket/remove', (req, res) => {
-    //
+app.delete('/basket/remove', (req, res) => {
+    console.log('Hi from /basket/remove' + req.cookies);
+    client.execute('SELECT * FROM store.basket;')
+        .then(result => console.log(result));
+    // Get cookie in req
+    // SELECT * FROM store.basket WHERE 
 });
 
 // Start the server
