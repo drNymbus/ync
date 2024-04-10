cad481490696703e32a2bb9a411768783b7b11de
nymbus <xxvmelanconxx@gmail.com>
2024-04-11 00:32:15 +0200

    Cleanup partially done, need further testing especially in cookie assertion. POSTMAN routine has been setup to get a copy do not hesitate to contact victor.melancon0@gmail.com.
    TO DO:
    - Handle code redundancy in routes
    - Cookie assertion needs to be stronger before any attemp to expose this
      service, undirectly this will make every routes a bit stronger

--- ync-node-app/js/routes.js
+++ ync-node-app/js/routes.js
@@ -16,6 +16,7 @@ const store_get = async (req, res, client) => {
 
                 await client.execute(utils.session.insert, [cookie, false, timestamp]); // create session
                 await client.execute(utils.basket.insert, [cookie]); // create basket
+
                 client.execute(utils.basket.select, [cookie]).then((result) => { // retrieve basket
                     res.cookie('ync_shop', cookie, {path: '/store', signed: true});
                     res.status(200).json(result.rows[0]); // send basket
@@ -51,8 +52,6 @@ const store_get = async (req, res, client) => {
 };
 exports.store_get = store_get;
 
-const execute_list_insert = async (client, ids, )
-
 const store_post = async (req, res, client) => {
     utils.log_query('post', req);
     // Retrieve cookie and add an element to the basket
@@ -60,23 +59,20 @@ const store_post = async (req, res, client) => {
     if (!utils.assert_cookie(client, cookie)) return utils.failed_request(res, 401, {'error': 'Invalid cookie'});
 
     if (req.query.basket === true) {
-        let ids = req.query.id.split(','); // transform query into cql query ready set values
-        for (let i in ids) ids[i] = `'${ids[i]}'`;
-        await client.execute(utils.basket.add_item, [ids, cookie]); // update basket with new item.s
-
+        await client.execute(utils.basket.add_item, [req.query.id.split(','), cookie]); // update basket with new item.s
         await client.execute(utils.session.update, [Date.now(), cookie]); // update session
+
         client.execute(utils.basket.select, [cookie]).then((result) => { // retrieve basket
             res.status(200).json(result.rows[0]); // send basket
         });
+
     } else if (req.query.item === true) {
         const ids = []; // Retrieve all fields from body
 
         for (let i in req.body) {
             let src_item = req.body[i], id = req.body[i].id; // item information
-
             let dst_item = []; // flatten item
             for (let [key, value] of Object.entries(src_item)) dst_item.push(value);
-
             await client.execute(utils.item.insert, dst_item) // add item to table
                 .then((result) => { // If statement correctly executed
                     ids.push(id); // add id to ids
@@ -84,29 +80,29 @@ const store_post = async (req, res, client) => {
         }
 
         await client.execute(utils.session.update, [Date.now(), cookie]); // update session
-        res.status(200).json({ids: ids}); // send ids inserted successfully
+        res.status(200).json({output: 'REQUEST COMPLETED', ids: ids}); // send ids inserted successfully
     }
 };
 exports.store_post = store_post;
 
-const store_delete = async (req, res) => {
+const store_delete = async (req, res, client) => {
     utils.log_query('delete', req);
     // Retrieve cookie and remove an element to the basket
     const cookie = req.signedCookies.ync_shop;
     if (!utils.assert_cookie(client, cookie)) utils.failed_request(res, 401, {'error': 'Invalid cookie'});
 
     if (req.query.basket === true) {
-        let ids = req.query.id.split(','); // transform query into cql query ready set values
-        for (let i in ids) ids[i] = `'${ids[i]}'`;
-        await client.execute(utils.basket.remove_item, [ids, cookie]); // update basket with new item.s
-
+        await client.execute(utils.basket.remove_item, [req.body.id.split(','), cookie]); // update basket with new item.s
         await client.execute(utils.session.update, [Date.now(), cookie]); // update session
+
         client.execute(utils.basket.select, [cookie]).then((result) => { // retrieve basket
             res.status(200).json(result.rows[0]); // send basket
         });
+
     } else if (req.query.item === true) {
         await client.execute(utils.item.delete, [req.query.id]); // update basket with item removed
         await client.execute(utils.session.update, [Date.now(), cookie]); // update session
+
         client.execute(utils.basket.select, [cookie]).then((result) => { // retrieve basket
             res.status(200).json(result.rows[0]); // send basket
         });
