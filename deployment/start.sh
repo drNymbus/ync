e2277741297bb3d13a1511988f0492fd1f675b5b
nymbus <xxvmelanconxx@gmail.com>
2024-04-18 00:45:11 +0200

    EVERYTHING IS WORKING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Still need to automate keyspace creation at pod startup, redact the README and have a cleaner start.sh

--- deployment/start.sh
+++ deployment/start.sh
@@ -2,21 +2,28 @@
 
 # Launch minikube
 minikube start
+
+# Tell minikube to use local docker repository
+
+# In UNIX
 eval $(minikube -p minikube docker-env)
 
-# For Powershell versions, either line will do the trick
-# minikube -p minikube docker-env --shell powershell | Invoke-Expression
-# minikube docker-env --shell powershell | Invoke-Expression
+# For Powershell versions
+minikube -p minikube docker-env --shell powershell | Invoke-Expression # In case an error is thrown, try removing 'minikube -p'
 
-minikube tunnel &
+# Run this command in another terminal to be able to access services from localhost
+minikube tunnel
 
 # Set namespace
 kubectl create namespace ync-app
-kubectl config set-context --current --namespace=ync-app
 
-# # Create a volume to contain all the initialization files for the cassandra cluster
-kubectl create configmap cassandra-cql --from-file=../ync-database/database-init/
+# Create a volume to contain all the initialization files for the cassandra cluster
+kubectl create configmap cassandra-cql --from-file=../ync-database/database-init/ --namespace=ync-app
 
-kubectl apply -f ./ync-service.yml # Create service to expose cassandra cluster's ports
-kubectl apply -f ./ync-database.yml # Launch the cassandra cluster
-kubectl apply -f ./ync-api.yml # Launch the node API
+kubectl apply -f ./ync-database.yml --namespace=ync-app
+kubectl apply -f ./ync-api.yml --namespace=ync-app
+# Create a service specifically for the node deployment
+kubectl expose deployment/node --type=NodePort --port=3000 --namespace=ync-app
+
+# To get access to the service on localhost
+minikube service node
