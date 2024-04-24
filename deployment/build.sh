d115741073acd8a42384a5547d4278c0ff6fa917
nymbus <xxvmelanconxx@gmail.com>
2024-04-24 23:51:37 +0200

    Build routine modified to register custom images to k3s daemon

--- deployment/build.sh
+++ deployment/build.sh
@@ -2,10 +2,16 @@
 
 cd ..
 
+# Build API image
 cd ync-node-app
 docker build -t ync-node-app .
 cd ..
 
+# Build React image
 cd ync-react-app
 docker build -t ync-react-app .
 cd ..
+
+# Register custom images to k3s
+docker save ync-node-app:latest | sudo k3s ctr images import -
+# docker save ync-react-app:latest | sudo k3s ctr images import -
