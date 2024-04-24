59e1f602fa62b40b564b45ddcba6491a90c7c83f
nymbus <xxvmelanconxx@gmail.com>
2024-04-24 23:47:42 +0200

    Added node api service in yaml to fix the exterior port to 15779. Need to open connection to WAN on Netero. Troubelshooting is happening now.

--- deployment/start.sh
+++ deployment/start.sh
@@ -8,5 +8,3 @@ kubectl create configmap cassandra-cql --from-file=../ync-database/database-init
 
 kubectl apply -f ./ync-database.yml --namespace=ync-app
 kubectl apply -f ./ync-api.yml --namespace=ync-app
-# Create a service specifically for the node deployment
-kubectl expose deployment/node --type=NodePort --port=3000 --namespace=ync-app
