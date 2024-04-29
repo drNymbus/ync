dcbe67da8e945d6e77f35de45df9aeaec9410e07
nymbus <xxvmelanconxx@gmail.com>
2024-04-29 02:04:28 +0200

    Build script fixed

--- deployment/k8s/build.sh
+++ deployment/k8s/build.sh
@@ -5,24 +5,27 @@ cd ../..
 # Build API images
 cd ync-api
 for api in `ls -d */`; do
-    echo "\n##############\nBuilding ${api}...\n##############\n"
+    echo "Building ${api::-1}..."
 
     cd ${api}
-    docker build -t ${api} .
-    docker save ${api}:latest | sudo k3s ctr images import -
+    docker build -t ${api::-1} .
+    docker save ${api::-1}:latest | sudo k3s ctr images import -
     cd ..
 
-    echo "\n##############\nSuccessfully built: ${api}\n##############\n\n"
+    echo "Successfully built: ${api::-1}."
 done
 cd ..
 
 # Build Application images
 cd ync-app
 for app in `ls -d */`; do
-    echo ${app}
+    echo "Building ${app::-1}..."
+
     cd ${app}
-    docker build -t ${app} .
+    docker build -t ${app::-1} .
     cd ..
+
+    echo "Successfully built: ${app::-1}."
 done
 cd ..
 
