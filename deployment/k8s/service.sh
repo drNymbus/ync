68407a7d182fa2bd23df934365f9e78e24374947
nymbus <xxvmelanconxx@gmail.com>
2024-05-15 15:54:59 +0200

    Transaction was failing because of too small 'prepared_statements_cache_size', switched to 50MiB works now. Notion updated about Cassandra really not a good choice for blob storage.

--- deployment/k8s/service.sh
+++ deployment/k8s/service.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-function component_status_check() {
+# Wait for a job to be complete
+function job_status_check() {
     while true; do
-        STATUS = $(kubectl get job "$1" -o=jsonpath='{.status.conditions[?(@.type=="Complete")].status}')
-        if [ $STATUS == "$2" ]; then
-            echo "$1 completed"
-            break
-        fi
+        STATUS = $(kubectl get job "$1" -o=jsonpath='{.status.conditions[?(@.type=="Complete")].type}')
+        if [ $STATUS == "Complete" ]; then break; fi
     done
 }
 
@@ -15,15 +13,14 @@ if [ "$1" == "start" ]; then
 
     # @desc: Create PersistentVolume and StatefulSet to run the database service
     if [ "$2" == "database" ]; then
-        # Init service
         kubectl apply -f ync-database/storage.yaml
         kubectl apply -f ync-database/database.yaml
-        component_status_check "cassandra-0" "Running"
+        sleep 90 # Sleep until database is fully initialized and operational
 
-        kubectl apply -f ync-database/init.yaml
-        component_status_check "ync-init-admin" "Complete"
-        kubectl apply -f ync-database/init.yaml
-        component_status_check "ync-init-keyspace" "Complete"
+        kubectl apply -f ync-database/job/admin.yaml
+        job_status_check "ync-init-admin" > /dev/k8s-job/job-admin.log
+        kubectl apply -f ync-database/job/keyspace.yaml
+        job_status_check "ync-init-keyspace" > /dev/k8s-job/job-keyspace.log
 
     # @desc: Create all apis
     elif [ "$2" == "api" ]; then
@@ -35,8 +32,17 @@ if [ "$1" == "start" ]; then
 
     # @desc: If no argument is provided, create all resources: database, apis and apps (in this particular order)
     else
-        for component in `ls ync-database/*.yaml`; do kubectl apply -f ${component}; done
-        init_cassandra
+        kubectl apply -f ync-database/storage.yaml
+        kubectl apply -f ync-database/database.yaml
+        sleep 90 # Sleep until database is fully initialized and operational
+
+        kubectl apply -f ync-database/job/admin.yaml
+        job_status_check "ync-init-admin" > /dev/k8s-job/job-admin.log
+        kubectl delete -f ync-database/job/admin.yaml
+
+        kubectl apply -f ync-database/job/keyspace.yaml
+        job_status_check "ync-init-keyspace" > /dev/k8s-job/job-keyspace.log
+        kubectl delete -f ync-database/job/keyspace.yaml
 
         for component in `ls ync-api/*.yaml`; do kubectl apply -f ${component}; done
         for component in `ls ync-app/*.yaml`; do kubectl apply -f ${component}; done
@@ -58,9 +64,23 @@ elif [ "$1" == "stop" ]; then
 elif [ "$1" == "delete" ]; then
     for component in `ls ync-app/*.yaml`; do kubectl delete -f ${component}; done
     for component in `ls ync-api/*.yaml`; do kubectl delete -f ${component}; done
+    kubectl delete -f ync-database/job/keyspace.yaml
+    kubectl delete -f ync-database/job/admin.yaml
     kubectl delete -f ync-database/database.yaml
+    kubectl delete -f ync-database/inspector.yaml
     kubectl delete -f ync-database/storage.yaml
 
 elif [ "$1" == "status" ]; then
     kubectl get all
+
+elif [ "$1" == "inspect" ]; then
+
+    if [ "$2" == "storage" ]; then
+        kubectl apply -f ync-database/inspector.yaml
+        kubectl exec -it pvc-inspector -- sh
+
+    elif [ "$2" == "database" ]; then
+        kubectl exec -it cassandra-0 -- /bin/bash
+    fi
+
 fi
