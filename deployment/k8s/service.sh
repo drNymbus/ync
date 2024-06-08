248b7d8171e367c5646c9164d9cbf0ff99cbc211
nymbus <xxvmelanconxx@gmail.com>
2024-06-08 15:11:10 +0200

    service.sh reduced to bare minimum

--- deployment/k8s/service.sh
+++ deployment/k8s/service.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Wait for a job to be complete
-function job_status_check() {
-    while true; do
-        STATUS = $(kubectl get job "$1" -o=jsonpath='{.status.conditions[?(@.type=="Complete")].type}')
-        if [ $STATUS == "Complete" ]; then break; fi
-    done
-}
-
-
 # @desc: Create resources to run the service, database, apis and apps can be ran separately
 if [ "$1" == "start" ]; then
     # Generate keys/secrets for apis and apps to run properly
@@ -20,13 +11,13 @@ if [ "$1" == "start" ]; then
     kubectl apply -f ync-database/job/admin.yaml
     kubectl apply -f ync-database/job/keyspace.yaml
 
-    for component in `ls ync-api/*.yaml`; do kubectl apply -f ${component}; done
-    for component in `ls ync-app/*.yaml`; do kubectl apply -f ${component}; done
+    for component in ./*.yaml; do kubectl apply -f ${component}; done
+    for component in ./*.yaml; do kubectl apply -f ${component}; done
 
 # @desc: Delete all resources
 elif [ "$1" == "delete" ]; then
-    for component in `ls ync-app/*.yaml`; do kubectl delete -f ${component}; done
-    for component in `ls ync-api/*.yaml`; do kubectl delete -f ${component}; done
+    for component in ./ync-app/*.yaml; do kubectl delete -f ${component}; done
+    for component in ./ync-api/*.yaml; do kubectl delete -f ${component}; done
 
     kubectl delete -f ync-database/job/keyspace.yaml
     kubectl delete -f ync-database/job/admin.yaml
