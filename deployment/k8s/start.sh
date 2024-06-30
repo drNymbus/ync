1abf74b785267df344d5f4a3fb39ceaa693769c1
nymbus <xxvmelanconxx@gmail.com>
2024-06-30 23:56:32 +0200

    Finding a way to distribute the certificate to every component that needs it

--- deployment/k8s/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "k3s" ]; then
-    systemctl start k3s
-elif [ "$1" == "minikube" ]; then
-    minikube start
-    eval $(minikube -p  minikube docker-env)
-    minikube tunnel
-fi
