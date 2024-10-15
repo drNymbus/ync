d5185df9ab34b4a762a2745163a67b93ae0d8b4f
nymbus <xxvmelanconxx@gmail.com>
2024-10-15 23:24:00 +0200

    Finally understood that K3s traefik is already pre-configured, what only remains is to correctly configure Ingress components on the cluster to distribute requests+path to correct services. More resources in docs/dns-reverse-proxy.md

--- cert-delivery/server.js
+++ cert-delivery/server.js
@@ -8,6 +8,6 @@ app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
 app.route('/').get((req, res) => { res.send("HELLO WORLD WIDE WEB!") });
 
-app.listen(80, () => {
+app.listen(8080, () => {
     console.log('HTTP server running on port 80');
 });
